178b326a565e7d77776db2b8a5816d25d2609601
michaelbag8 <michaelbag8@gmail.com>
2026-05-23 22:16:06 +0100

    add:createBook

---
+++ go.mod
@@ -0,0 +1,3 @@
+module books-rest-api
+
+go 1.22.2
--- main.go
+++ main.go
@@ -6,19 +6,17 @@ import (
 	"net/http"
 )
 
-
-
 type Book struct {
-    ID     int    `json:"id"`
-    Title  string `json:"title"`
-    Author string `json:"author"`
-    Pages  int    `json:"pages"`
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
 }
 
-//this will serve as my database fro now
+// this will serve as my database for now
 var books = []Book{
-    {ID: 1, Title: "The Go Programming Language", Author: "Alan Donovan", Pages: 380},
-    {ID: 2, Title: "Clean Code", Author: "Robert Martin", Pages: 431},
+	{ID: 1, Title: "The Go Programming Language", Author: "Alan Donovan", Pages: 380},
+	{ID: 2, Title: "Clean Code", Author: "Robert Martin", Pages: 431},
 	{ID: 3, Title: "Start With Why", Author: "Simon Sinek", Pages: 401},
 	{ID: 4, Title: "Breaking Out", Author: "Michael Bag", Pages: 500},
 }
@@ -26,87 +24,120 @@ var books = []Book{
 // since we already have four books our next will be 5
 var nextID int = 5
 
-func writeJSON(w http.ResponseWriter, status int, data any){
+func writeJSON(w http.ResponseWriter, status int, data any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
+
 	json.NewEncoder(w).Encode(data)
 
 }
 
-
-func getAllBooks(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodGet{
-
+//getAllBooks to get the list of all the books
+func getAllBooks(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{
+			"error": "method not allowed",
+		})
+		return
 	}
-	
-}
-func updateBook(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodGet{
 
-	}
-	
-}
-func getBook(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodGet{
-
-	}
-	
-}
-func createBook(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodGet{
-
-	}
-	
-}
-func deleteBook(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodGet{
-
-	}
-	
+	writeJSON(w, http.StatusOK, books)
 }
 
+func createBook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{
+			"error": "method not allowed",
+		})
+		return
+	}
+	var req Book
+	if req.Title == "" || req.Author == "" || req.Pages == 0{
+		writeJSON(w, http.StatusBadRequest, map[string]string{
+			"error": "title, author and pages are required",
+		})
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err!=nil{
+		writeJSON(w, http.StatusBadRequest, map[string]string{
+			"error": "invalid request body",
+		})
+		return
+	}
+
+	req.ID = nextID
+	nextID++
+	books = append(books, req)
+	writeJSON(w, http.StatusCreated, req)
+
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{
+			"error": "method not allowed",
+		})
+		return
+	}
+
+}
+func getBook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+
+	}
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+
+	}
+
+}
 
 // to handler all books method
-func booksHandler(w http.ResponseWriter, r *http.Request){
-	switch r.Method{
+func booksHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
 	case http.MethodGet:
 		getAllBooks(w, r)
 	case http.MethodPost:
-		createBook(w,r)
+		createBook(w, r)
 	default:
 		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{
 			"error": "method not allowed",
-		})	
+		})
 	}
-	
+
 }
 
-
-//// to handler single book method
-func bookHandler(w http.ResponseWriter, r *http.Request){
-	switch r.Method{
+// // to handler single book method
+func bookHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
 	case http.MethodGet:
 		getBook(w, r)
 	case http.MethodPut:
-		updateBook(w,r)
+		updateBook(w, r)
 	case http.MethodDelete:
-		deleteBook(w,r)
+		deleteBook(w, r)
 	default:
 		writeJSON(w, http.StatusMethodNotAllowed, map[string]string{
 			"error": "method not allowed",
-		})	
+		})
 	}
-	
+
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/books", booksHandler)
 	http.HandleFunc("/book", bookHandler)
 
 	fmt.Println("serving is runing at http://localhost:8080/")
-	server := http.NewServeMux()
-	if err := http.ListenAndServe("8080", server); err != nil{
-		fmt.Println("server failed to start")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("server failed to start",err)
 		return
 	}
 }
+
